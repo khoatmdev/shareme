7f8b404b3c56d1790294dfdda7b31bd2bb6151d7
KhoaTM <khoaanhdat1412@gmail.com>
2022-10-23 02:48:31 +0700

    [FE] Implement google login

---
+++ shareme_frontend/src/components/Login.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { GoogleLogin } from "react-google-login";
+import { useNavigate } from "react-router-dom";
+import { FcGoogle } from "react-icons/fc";
+
+import shareVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+
+const Login = () => {
+  const handleResponseGoogle = (response) => {
+    console.log("res: ", response);
+  };
+
+  return (
+    <div className="flex flex-col justify-start items-center h-screen">
+      <div className="relative w-full h-full">
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
+
+        <div className="absolute flex flex-col justify-center items-center top-0 left-0 right-0 bottom-0 bg-black bg-opacity-40">
+          <div className="p-5">
+            <img src={logo} width="130px" alt="logo" />
+          </div>
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}
+              render={(props) => (
+                <button
+                  type="button"
+                  className="bg-white flex justify-center items-center p-2 rounded-sm cursor-pointer outline-none"
+                  onClick={props.onClick}
+                  disabled={props.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign In With Google
+                </button>
+              )}
+              onSuccess={handleResponseGoogle}
+              onFailure={handleResponseGoogle}
+              cookiePolicy={"single_host_origin"}
+              scope="email"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- shareme_frontend/src/index.js
+++ shareme_frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
+import "react-app-polyfill/ie11";
 
 import App from "./App";
 import "./index.css";
