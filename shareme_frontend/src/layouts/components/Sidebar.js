9db33fda4ffcfefde2a75ad698532886de04bc13
KhoaTM <khoaanhdat1412@gmail.com>
2022-10-26 01:03:04 +0700

    [FE] LAYOUT: Implement Sidebar

--- shareme_frontend/src/layouts/components/Sidebar.js
+++ shareme_frontend/src/layouts/components/Sidebar.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '../../assets/logo.png';
 
 const Sidebar = () => {
   return (
-    <>
-      <div>Sidebar</div>
-    </>
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-auto min-w-[210px]">
+      <div className="flex flex-col">
+        <Link to={'/'} className="flex px-5 my-6 gap-2 pt-1 w-190 items-center">
+          <img src={logo} alt="logo" />
+        </Link>
+      </div>
+    </div>
   );
 };
 
